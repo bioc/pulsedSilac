c9973acd7dd0ed0eba52a9c272dd0b67d03ea5b9
lshep <lori.shepherd@roswellpark.org>
2023-01-25 10:44:07 -0500

    Deprecate  - unresponsive

--- R/zzz.R
+++ R/zzz.R
@@ -323,3 +323,10 @@ NULL
 
 
 
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.18")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
+
